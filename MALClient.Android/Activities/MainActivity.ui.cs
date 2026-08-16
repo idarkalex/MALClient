60c400a2e7a1a31e736826fcbdb2b8cc935f1e05
idarkalex <alexblancocostal@gmail.com>
2026-08-16 19:08:53 +0200

    Fix: try IOnItemSelectedListener with SetOnItemSelectedListener

--- MALClient.Android/Activities/MainActivity.ui.cs
+++ MALClient.Android/Activities/MainActivity.ui.cs
@@ -351,7 +351,7 @@ namespace MALClient.Android.Activities
             MainPageHamburgerButton.Visibility = ViewStates.Gone;
             _drawer.DrawerLayout.SetDrawerLockMode(DrawerLayout.LockModeLockedClosed);
 
-            MainPageBottomNav.SetOnNavigationItemSelectedListener(new BottomNavigationItemSelectedListener(OnBottomNavigationItemSelected));
+            MainPageBottomNav.SetOnItemSelectedListener(new BottomNavigationItemSelectedListener(OnBottomNavigationItemSelected));
 
             MainPageCloseVideoButton.SetZ(0);
             MainPageCopyVideoLinkButton.SetZ(0);
--- MALClient.Android/Listeners/BottomNavigationItemSelectedListener.cs
+++ MALClient.Android/Listeners/BottomNavigationItemSelectedListener.cs
@@ -1,10 +1,9 @@
 using System;
-using Android.Support.Design.Widget;
 using Android.Views;
 
 namespace MALClient.Android.Listeners
 {
-    public class BottomNavigationItemSelectedListener : Java.Lang.Object, BottomNavigationView.IOnNavigationItemSelectedListener
+    public class BottomNavigationItemSelectedListener : Java.Lang.Object, Android.Support.Design.Widget.BottomNavigationView.IOnItemSelectedListener
     {
         private readonly Func<IMenuItem, bool> _action;
 
