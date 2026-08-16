b9a862e76bf6b017ee25b399e52bc1974ba4cfdc
idarkalex <alexblancocostal@gmail.com>
2026-08-16 19:57:44 +0200

    Revert to SetOnClickListener, ItemSelected event not available in Support Library 28

--- MALClient.Android/Activities/MainActivity.ui.cs
+++ MALClient.Android/Activities/MainActivity.ui.cs
@@ -351,7 +351,7 @@ namespace MALClient.Android.Activities
                         MainPageHamburgerButton.Visibility = ViewStates.Gone;
             _drawer.DrawerLayout.SetDrawerLockMode(DrawerLayout.LockModeLockedClosed);
 
-            MainPageBottomNav.ItemSelected += (sender, e) => OnBottomNavigationItemSelected(e.Item.ItemId);
+            MainPageBottomNav.SetOnClickListener(new OnClickListener(view => OnBottomNavigationItemSelected(MainPageBottomNav.SelectedItemId)));
 
             MainPageCloseVideoButton.SetZ(0);
             MainPageCopyVideoLinkButton.SetZ(0);
