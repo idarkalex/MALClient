9dff2510b272d49efa0aef5833d6321e4c8a3cc6
idarkalex <alexblancocostal@gmail.com>
2026-08-16 19:17:29 +0200

    Fix: use [Register] attribute for Java method mapping, use IOnNavigationItemSelectedListener

--- MALClient.Android/Activities/MainActivity.ui.cs
+++ MALClient.Android/Activities/MainActivity.ui.cs
@@ -351,7 +351,7 @@ namespace MALClient.Android.Activities
             MainPageHamburgerButton.Visibility = ViewStates.Gone;
             _drawer.DrawerLayout.SetDrawerLockMode(DrawerLayout.LockModeLockedClosed);
 
-            MainPageBottomNav.SetOnItemSelectedListener(new BottomNavigationItemSelectedListener(OnBottomNavigationItemSelected));
+            MainPageBottomNav.SetOnNavigationItemSelectedListener(new BottomNavigationItemSelectedListener(OnBottomNavigationItemSelected));
 
             MainPageCloseVideoButton.SetZ(0);
             MainPageCopyVideoLinkButton.SetZ(0);
--- MALClient.Android/Listeners/BottomNavigationItemSelectedListener.cs
+++ MALClient.Android/Listeners/BottomNavigationItemSelectedListener.cs
@@ -1,9 +1,10 @@
 using System;
+using Android.Runtime;
 using Android.Views;
 
 namespace MALClient.Android.Listeners
 {
-    public class BottomNavigationItemSelectedListener : Java.Lang.Object, global::Android.Support.Design.Widget.BottomNavigationView.IOnItemSelectedListener
+    public class BottomNavigationItemSelectedListener : Java.Lang.Object, global::Android.Support.Design.Widget.BottomNavigationView.IOnNavigationItemSelectedListener
     {
         private readonly Func<IMenuItem, bool> _action;
 
@@ -12,6 +13,7 @@ namespace MALClient.Android.Listeners
             _action = action;
         }
 
+        [Register("onNavigationItemSelected", "(Landroid/view/MenuItem;)Z", "GetOnNavigationItemSelected_Landroid_view_MenuItem_Handler")]
         public bool OnNavigationItemSelected(IMenuItem item)
         {
             return _action?.Invoke(item) ?? true;
