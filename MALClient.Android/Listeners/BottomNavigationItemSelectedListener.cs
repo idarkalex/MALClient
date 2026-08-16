dc8e91c0ff3c85e349c57890ce2e23ec22ac41c1
idarkalex <alexblancocostal@gmail.com>
2026-08-16 19:12:36 +0200

    Fix: use global:: to resolve Android namespace in Listeners namespace

--- MALClient.Android/Listeners/BottomNavigationItemSelectedListener.cs
+++ MALClient.Android/Listeners/BottomNavigationItemSelectedListener.cs
@@ -3,7 +3,7 @@ using Android.Views;
 
 namespace MALClient.Android.Listeners
 {
-    public class BottomNavigationItemSelectedListener : Java.Lang.Object, Android.Support.Design.Widget.BottomNavigationView.IOnItemSelectedListener
+    public class BottomNavigationItemSelectedListener : Java.Lang.Object, global::Android.Support.Design.Widget.BottomNavigationView.IOnItemSelectedListener
     {
         private readonly Func<IMenuItem, bool> _action;
 
