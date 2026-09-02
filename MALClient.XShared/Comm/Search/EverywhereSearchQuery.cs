47c9a0316d534ee68e5eb2e1320eeadd714deedd
idarkalex <alexblancocostal@gmail.com>
2026-09-02 13:37:40 +0200

    F5b: EverywhereSearch prefix->Tenrai multi-q (anime/manga/characters) + F4/F6 already pushed

--- MALClient.XShared/Comm/Search/EverywhereSearchQuery.cs
+++ MALClient.XShared/Comm/Search/EverywhereSearchQuery.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using MALClient.Models.Models.ApiResponses;
+using MALClient.XShared.Comm;
 using Newtonsoft.Json;
 
 namespace MALClient.XShared.Comm.Search
@@ -14,28 +16,69 @@ namespace MALClient.XShared.Comm.Search
         {
             try
             {
+                var tenraiTasks = new List<Task<List<Item>>>();
+                var categories = new List<Category>();
+
+                try
+                {
+                    var animeTask = SearchTenraiCategory("anime", query, "anime");
+                    var mangaTask = SearchTenraiCategory("manga", query, "manga");
+                    var charTask = SearchTenraiCategory("characters", query, "character");
+                    await Task.WhenAll(animeTask, mangaTask, charTask);
+                    if (animeTask.Result.Count > 0) categories.Add(new Category { Type = "anime", Items = animeTask.Result });
+                    if (mangaTask.Result.Count > 0) categories.Add(new Category { Type = "manga", Items = mangaTask.Result });
+                    if (charTask.Result.Count > 0) categories.Add(new Category { Type = "character", Items = charTask.Result });
+                }
+                catch { }
+
+                try
+                {
+                    var jsonUser = await _client.GetStringAsync(
+                        $"https://myanimelist.net/search/prefix.json?type=user&keyword={query}&v=1");
+                    var userResponse = JsonConvert.DeserializeObject<SearchEverywhereResponse>(jsonUser);
+                    if (userResponse?.Categories?.Any() == true)
+                    {
+                        userResponse.Categories[0].Items =
+                            userResponse.Categories[0].Items.OrderByDescending(item => item.EsScore).Take(5).ToList();
+                        categories.Add(userResponse.Categories.First());
+                    }
+                }
+                catch { }
+
+                if (categories.Count > 0)
+                    return new SearchEverywhereResponse { Categories = categories };
+
                 var json = await _client.GetStringAsync(
                     $"https://myanimelist.net/search/prefix.json?type=all&keyword={query}&v=1");
-                var jsonUser = await _client.GetStringAsync(
-                    $"https://myanimelist.net/search/prefix.json?type=user&keyword={query}&v=1");
-
-                var userReponse = JsonConvert.DeserializeObject<SearchEverywhereResponse>(jsonUser);
-
-                var response = JsonConvert.DeserializeObject<SearchEverywhereResponse>(json);
-
-                if (userReponse.Categories.Any())
-                {
-                    userReponse.Categories[0].Items =
-                        userReponse.Categories[0].Items.OrderByDescending(item => item.EsScore).Take(5).ToList();
-                    response.Categories.Add(userReponse.Categories.First());
-                }
-
-                return response;
+                return JsonConvert.DeserializeObject<SearchEverywhereResponse>(json);
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 return null;
             }
+        }
+
+        private async Task<List<Item>> SearchTenraiCategory(string endpoint, string query, string type)
+        {
+            try
+            {
+                var (items, _) = await TenraiClient.GetPaginatedAsync($"{endpoint}?q={Uri.EscapeDataString(query)}&sfw");
+                return items.Take(5).Select(el =>
+                {
+                    var id = el.TryGetProperty("mal_id", out var p) && p.ValueKind == System.Text.Json.JsonValueKind.Number ? p.GetInt32() : 0;
+                    var title = el.TryGetProperty("title", out var tp) && tp.ValueKind == System.Text.Json.JsonValueKind.String ? tp.GetString() : "";
+                    var img = "";
+                    if (el.TryGetProperty("images", out var imgs) && imgs.ValueKind == System.Text.Json.JsonValueKind.Object)
+                    {
+                        if (imgs.TryGetProperty("jpg", out var jpg) && jpg.ValueKind == System.Text.Json.JsonValueKind.Object)
+                            if (jpg.TryGetProperty("image_url", out var url) && url.ValueKind == System.Text.Json.JsonValueKind.String)
+                                img = url.GetString();
+                    }
+                    var urlStr = $"https://myanimelist.net/{type}/{id}";
+                    return new Item { Id = id, Type = type, Name = title, Url = urlStr, ImageUrl = img, ThumbnailUrl = img, EsScore = 1.0, Payload = new Payload() };
+                }).Where(i => i.Id > 0 && !string.IsNullOrEmpty(i.Name)).ToList();
+            }
+            catch { return new List<Item>(); }
         }
     }
 }
