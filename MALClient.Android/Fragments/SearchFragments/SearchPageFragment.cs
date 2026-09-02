7912ae162778b86901b4af3763be83ca604eaa76
idarkalex <alexblancocostal@gmail.com>
2026-09-02 15:17:35 +0200

    SearchPage fixed bar + dropdown CurrentHintSet + Discover fixed bar (More->Search only)

--- MALClient.Android/Fragments/SearchFragments/SearchPageFragment.cs
+++ MALClient.Android/Fragments/SearchFragments/SearchPageFragment.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
 using Android.Support.V4.View;
@@ -15,6 +16,7 @@ using MALClient.Android.Resources;
 
 using MALClient.Models.Enums;
 using MALClient.XShared.NavArgs;
+using MALClient.XShared.Utils;
 using MALClient.XShared.ViewModels;
 
 namespace MALClient.Android.Fragments.SearchFragments
@@ -38,6 +40,43 @@ namespace MALClient.Android.Fragments.SearchFragments
 
             SearchPageViewPager.SetCurrentItem(start, false);
             HasOnlyManualBindings = true;
+
+            SearchPageSearchView.Iconified = false;
+            if (!string.IsNullOrEmpty(_args?.Query))
+                SearchPageSearchView.SetQuery(_args.Query, false);
+            var hintAdapter = new ArrayAdapter<string>(Activity, global::Android.Resource.Layout.SimpleDropDownItem1Line);
+            var autoComplete = SearchPageSearchView.FindViewById<AutoCompleteTextView>(Resource.Id.search_src_text);
+            if (autoComplete != null)
+            {
+                autoComplete.Adapter = hintAdapter;
+                autoComplete.Threshold = 1;
+                autoComplete.SetTextColor(Color.White);
+            }
+            SearchPageSearchView.QueryTextChange += (s, e) =>
+            {
+                ViewModelLocator.GeneralMain.CurrentSearchQuery = e.NewText;
+                hintAdapter.Clear();
+                foreach (var h in ViewModelLocator.GeneralMain.CurrentHintSet ?? new System.Collections.Generic.List<string>())
+                    hintAdapter.Add(h);
+                hintAdapter.NotifyDataSetChanged();
+            };
+            SearchPageSearchView.QueryTextSubmit += (s, e) =>
+            {
+                var q = SearchPageSearchView.Query?.ToString()?.Trim();
+                if (!string.IsNullOrEmpty(q) && q.Length >= 2)
+                {
+                    _args.Query = q;
+                    _args.ForceQuery = true;
+                    ViewModelLocator.GeneralMain.CurrentSearchQuery = q;
+                    SearchPageViewPager.Adapter = new SearchPagePagerAdapter(ChildFragmentManager, _args, out int ns);
+                    SearchPageTabStrip.SetViewPager(SearchPageViewPager);
+                    SearchPageViewPager.SetCurrentItem(0, false);
+                }
+                e.Handled = true;
+                SearchPageSearchView.ClearFocus();
+            };
+            var se = SearchPageSearchView.FindViewById(Resource.Id.search_src_text) as EditText;
+            if (se != null) se.SetTextColor(Color.White);
         }
 
         protected override void Init(Bundle savedInstanceState)
@@ -49,10 +88,13 @@ namespace MALClient.Android.Fragments.SearchFragments
 
         private UserControls.PagerSlidingTabStrip _searchPageTabStrip;
         private ViewPager _searchPageViewPager;
+        private global::Android.Support.V7.Widget.SearchView _searchPageSearchView;
 
         public UserControls.PagerSlidingTabStrip SearchPageTabStrip => GetView(ref _searchPageTabStrip, Resource.Id.SearchPageTabStrip);
 
         public ViewPager SearchPageViewPager => GetView(ref _searchPageViewPager, Resource.Id.SearchPageViewPager);
+
+        public global::Android.Support.V7.Widget.SearchView SearchPageSearchView => GetView(ref _searchPageSearchView, Resource.Id.SearchPageSearchView);
 
         #endregion
 
